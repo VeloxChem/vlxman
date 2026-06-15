3d6ea3fbb5ab37114e5151d21a1dd4c727892f3c
mathieulinares <linares@kth.se>
2026-06-15 13:41:08 +0200

    fix general response with HOOH geometry + input files

--- input_files/h2o2-lrf.py
+++ input_files/h2o2-lrf.py
@@ -1,14 +1,22 @@
 import veloxchem as vlx
 
-molecule = vlx.Molecule.read_smiles("OO")
+molecule = vlx.Molecule.read_xyz_string("""4
+Hydrogen peroxide
+O  -0.65564532 -0.06106286 -0.03621403
+O   0.65564532  0.06106286 -0.03621403
+H  -0.97628735  0.65082652  0.57474201
+H   0.97628735 -0.65082652  0.57474201
+""")
 basis = vlx.MolecularBasis.read(molecule, "def2-svpd")
 
 scf_drv = vlx.ScfRestrictedDriver()
+scf_drv.filename="h2o2-lrf"
 
 scf_drv.xcfun = "b3lyp"
 scf_results = scf_drv.compute(molecule, basis)
 
 crs = vlx.ComplexResponse()
+crs.filename="h2o2-lrf"
 
 # available operators
 # crs.b_operator = "electric dipole"
--- input_files/h2o2-qrf.py
+++ input_files/h2o2-qrf.py
@@ -1,14 +1,22 @@
 import veloxchem as vlx
 
-molecule = vlx.Molecule.read_smiles("OO")
+molecule = vlx.Molecule.read_xyz_string("""4
+Hydrogen peroxide
+O  -0.65564532 -0.06106286 -0.03621403
+O   0.65564532  0.06106286 -0.03621403
+H  -0.97628735  0.65082652  0.57474201
+H   0.97628735 -0.65082652  0.57474201
+""")
 basis = vlx.MolecularBasis.read(molecule, "def2-svpd")
 
 scf_drv = vlx.ScfRestrictedDriver()
+scf_drv.filename="h2o2-qrf"
 
 scf_drv.xcfun = "b3lyp"
 scf_results = scf_drv.compute(molecule, basis)
 
 qrf_drv = vlx.QuadraticResponseDriver()
+qrf_drv.filename="h2o2-qrf"
 
 qrf_drv.a_operator = "electric dipole"
 qrf_drv.b_operator = "magnetic dipole"
